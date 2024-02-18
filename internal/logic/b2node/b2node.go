55c6f928d06c6cb7bcee560022e5702eddd8668a
oxf71 <144757737+oxf71@users.noreply.github.com>
2024-02-18 14:18:31 +0800

    fix: nonces repeat

--- internal/logic/b2node/b2node.go
+++ internal/logic/b2node/b2node.go
@@ -6,6 +6,7 @@ import (
 	"fmt"
 	"strconv"
 	"strings"
+	"sync"
 
 	"github.com/b2network/b2-indexer/internal/types"
 	"github.com/b2network/b2-indexer/pkg/log"
@@ -35,6 +36,10 @@ const (
 
 	EventTypeCreateDeposit = "EventCreateDeposit"
 	EventTypeSignWithdraw  = "EventSignWithdraw"
+)
+
+var (
+	txLock sync.Mutex
 )
 
 type NodeClient struct {
@@ -120,6 +125,8 @@ func (n *NodeClient) GetGasPrice() (uint64, error) {
 }
 
 func (n *NodeClient) broadcastTx(ctx context.Context, msgs ...sdk.Msg) (*tx.BroadcastTxResponse, error) {
+	txLock.Lock()
+	defer txLock.Unlock()
 	gasPrice, err := n.GetGasPrice()
 	if err != nil {
 		return nil, fmt.Errorf("[broadcastTx][GetEthGasPrice] err: %s", err)
